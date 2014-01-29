52d53ef3ae67118140a847ae1f16ccc027582309
Ryan Phillips <wordsandcolor@gmail.com>
2014-01-29 12:21:57 -0800

    added more test cases

--- hw2/test_scanner.py
+++ hw2/test_scanner.py
@@ -11,6 +11,8 @@ def test_word(_in,_pass,_type):
 	if (_pass):
 		assert(t[0][0][0]==_type)
 		assert(t[0][0][1]==_in)
+	return t # just in case we need it
+
 
 def test_all():
 
@@ -19,9 +21,49 @@ def test_all():
 	test_word('113535',True,'int_number')
 	test_word('Crazy123_Var__a',True,'ID')
 
+	#key words that should be recognized
+	test_word('true',True,'bool_const')
+	test_word('true',True,'bool_const')
+	test_word('and',True,'log_op')
+	test_word('or',True,'log_op')
+	test_word('not',True,'log_op')
+	test_word('sin',True,'trig_op')
+	test_word('cos',True,'trig_op')
+	test_word('tan',True,'trig_op')
+
+	test_word('bool',True,'keyword')
+	test_word('let',True,'keyword')
+	test_word('real',True,'keyword')
+	test_word('string',True,'keyword')
+	test_word('stdout',True,'keyword')
+	test_word('if',True,'keyword')
+	test_word('then',True,'keyword')
+	test_word('while',True,'keyword')
+	test_word('for',True,'keyword')
+
+	#the following should not register as any of the above
+	#keyword types, but instead as IDs
+
+	ids_to_check = []
+	ids_to_check.append(test_word('True',True,'ID'))
+	ids_to_check.append(test_word('FALSE',True,'ID'))
+	ids_to_check.append(test_word('anD',True,'ID'))
+	ids_to_check.append(test_word('Sin',True,'ID'))
+	ids_to_check.append(test_word('Cos',True,'ID'))
+	ids_to_check.append(test_word('Tan',True,'ID'))
+	ids_to_check.append(test_word('BOOL',True,'ID'))
+	# etc....
+
+	#we will also check to ensure all the IDs have been added to
+	#the symbol table
+
+	for i in xrange(len(ids_to_check)):
+		assert(ids_to_check[i][0][0][1] in symbol_table)
+	
 	#bad words
 	test_word('1.423.23',False,'')
 	test_word('.090..',False,'')
+	test_word('?',False,'')
 
 	# what about 1AtestGoodOrBadVar ? not a valid id, but should it parse
 	# as a int_number and then an ID
