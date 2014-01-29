c6628f60f08eb3cebead66c4c76a87047fdc06b7
Ryan Phillips <wordsandcolor@gmail.com>
2014-01-29 12:47:01 -0800

    t

--- hw2/test_scanner.py
+++ hw2/test_scanner.py
@@ -13,11 +13,14 @@ def test_word(_in,_pass,_type):
 		assert(t[0][0][1]==_in)
 	return t # just in case we need it
 
-def test_sentence(_in):
+def test_sentence(_in,_pass):
 	# to be finished... this might be a bit simpler than test_word,
 	# maybe just a call to: scanner(whole sentence)
 	t = scanner(_in)
-	assert(t[1]) 
+	if (_pass):
+		assert(t[1]) 
+	else:
+		assert(t[1] == False)
 	return t
 
 def test_all():
@@ -82,7 +85,23 @@ def test_list_of_words():
 #we will call these, 'good sentences'
 def test_list_of_sentences():
 	#good sentences
-	test_sentence('ok + 1 + 3')
+	###############
+
+	test_sentence('if [bool x = True] then y = 5.234',True)
+	test_sentence('1+4*8^3/4%1.534-.13409',True)
+
+	#bad sentences
+	##############
+
+	#umm... should this parse as float, .0984 float .9089,
+	#or read it as a single number and fail because it contains two
+	#decimals?
+	#i'm guessing it should fail
+	test_sentence('.0984.9089 + .9083-.89384',False)
+
+
+
+
 
 
 
