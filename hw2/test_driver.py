22dbe7c1384ecb42aa5aafaaebf7f10a46e32636
Gregorio Luis Ramirez <luisramg@onid.orst.edu>
2014-01-30 22:31:47 -0800

    adding new test cases in test driver

--- hw2/test_driver.py
+++ hw2/test_driver.py
@@ -34,9 +34,7 @@ class TestSpecialCases(unittest.TestCase):
         
         # Assert
         self.assertEqual(expected_tokens, actual_tokens[0])
-     
-    # def test_keywords_recognized_before_ids:
-    
+         
     def test3_comment_lines(self):
         # Arrange
         _input = "// this is a comment line "
@@ -67,6 +65,89 @@ class TestSpecialCases(unittest.TestCase):
                            ('bracket-r',']')]
         actual_tokens = scanner(_input)
         self.assertEqual(expected_tokens, actual_tokens[0])
+    
+    def test6_test_identifiers_vs_keywords(self):
+        _input = '[let [[a int][b real]]]'
+        expected_tokens = [
+                           ('bracket-l','['),
+                           ('keyword','let'),
+                           ('bracket-l','['),
+                           ('bracket-l','['),
+                           ('ID','a'),
+                           ('keyword','int'),
+                           ('bracket-r',']'),
+                           ('bracket-l','['),
+                           ('ID','b'),
+                           ('keyword','real'),
+                           ('bracket-r',']'),
+                           ('bracket-r',']'),
+                           ('bracket-r',']'),
+                           ]
+        actual_tokens = scanner(_input)
+        self.assertEqual(expected_tokens, actual_tokens[0])
+        
+    def test7_test_assignment_operator(self):
+        _input = '[:= x 123][:= y 1.23][:= z .123]'
+        expected_tokens = [
+                           ('bracket-l','['),
+                           ('assignment_op',''),
+                           ('ID','x'),
+                           ('int_number','123'),
+                           ('bracket-r',']'),
+                           
+                           ('bracket-l','['),
+                           ('assignment_op',''),
+                           ('ID','y'),
+                           ('real_number','1.23'),
+                           ('bracket-r',']'),
+                           
+                           ('bracket-l','['),
+                           ('assignment_op',''),
+                           ('ID','z'),
+                           ('real_number','.123'),
+                           ('bracket-r',']'),
+                           ]
+        actual_tokens = scanner(_input)
+        self.assertEqual(expected_tokens, actual_tokens[0])
+    
+class TestExpressions(unittest.TestCase):
+    def test8_test_unary_operators(self):
+        _input = '[sin x]'
+        expected_tokens = [('bracket-l','['),
+                       ('trig_op','sin'),
+                       ('ID','x'),
+                       ('bracket-r',']')]
+        actual_tokens = scanner(_input)
+        self.assertEqual(expected_tokens, actual_tokens[0])
+                           
+        expected_output = [()]
+        
+    def test9_test_binary_expressions(self):
+        _input = '[+ 1 [* 2 3]]'
+        expected_tokens = [('bracket-l','['),
+                       ('arithmatic_op','+'),
+                       ('int_number','1'),
+                       ('bracket-l','['),
+                       ('arithmatic_op','*'),
+                       ('int_number','2'),
+                       ('int_number','3'),
+                       ('bracket-r',']'),
+                       ('bracket-r',']')]
+        actual_tokens = scanner(_input)
+        self.assertEqual(expected_tokens, actual_tokens[0])
+        
+class TestInvalidTokens(unittest.TestCase):
+    def test10_test_possible_invalid_string_constants(self):
+        _input = '"hello world'
+        expected_output = []
+        actual_output = scanner(_input)
+        self.assertEqual(expected_output, actual_output[0])
+    
+    def test11_test_possible_invalid_identifier_name(self):
+        _input = '2varName'
+        expected_output = [('int_number', '2'), ('ID', 'varName')]
+        actual_output = scanner(_input)
+        self.assertEqual(expected_output, actual_output[0])
         
 if __name__ == '__main__':
     
