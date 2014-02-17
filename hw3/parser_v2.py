9a99ffc3a2607c4261371146708560cb750b3b38
Ryan Phillips <wordsandcolor@gmail.com>
2014-02-17 15:17:48 -0800

    alright, the parser spits out a tree correctly for most test cases now

--- hw3/parser_v2.py
+++ hw3/parser_v2.py
@@ -74,7 +74,9 @@ def oper(x,d):
 				ret1 = name([x[2]],d) 
 				ret2 = oper(x[3:-1],d)
 				if (ret1 != None and ret2 != None):
-					new_node = Node("TODO")
+					new_node = Node("oper->[:= name oper]")
+					new_node.add_child(ret1[1])
+					new_node.add_child(ret2[1])
 					return 'oper->[:= name oper],' + ret1[0] + ret2[0], new_node
 
 			ret1 = binops([x[1]],d)
@@ -94,7 +96,9 @@ def oper(x,d):
 			ret1 = unops([x[1]],d)
 			ret2 = oper([x[2]],d)
 			if (ret1 != None and ret2 != None):
-				new_node = Node("TODO1")
+				new_node = Node("oper->[unops oper]")
+				new_node.add_child(ret1[1])
+				new_node.add_child(ret2[1])
 				return 'oper->[unops oper],' + ret1[0] + ret2[0], new_node
 
 	ret = constants(x,d)
@@ -107,7 +111,7 @@ def oper(x,d):
 	if ret != None:
 		new_node = Node("oper->name")
 		new_node.add_child(ret[1])
-		return 'oper->name,' + ret[1], new_node
+		return 'oper->name,' + ret[0], new_node
 
 
 def binops(x,d):
@@ -117,11 +121,15 @@ def binops(x,d):
 
 def unops(x,d):
 	if x[0][0] in ['trig_op','log_op']:
-		return 'unops->'+x[0][1]+','
+		new_node = Node(x[0][1])
+		return 'unops->'+x[0][1]+',', new_node
 
 def constants(x,d):
 	ret = strings(x,d)
-	if ret != None: return 'constants->strings,' + ret
+	if ret != None:
+		new_node = Node("constants->strings")
+		new_node.add_child(ret[1])
+		return 'constants->strings,' + ret[0], new_node
 
 	ret = ints(x,d)
 	if ret != None:
@@ -137,12 +145,14 @@ def constants(x,d):
 
 def strings(x,d):
 	if (len(x) == 1):
-		if x[0][0] == 'string': return 'strings->STRINGS'
+		if x[0][0] == 'string':
+			new_node = Node(x[0][1])
+			return 'strings->STRINGS', new_node
 
 def name(x,d):
 	if (len(x) == 1):
-		new_node = Node(x[0][1])
 		if x[0][0] == 'ID':
+			new_node = Node(x[0][1])
 			return 'name->NAME,', new_node
 
 def ints(x,d):
@@ -159,23 +169,38 @@ def floats(x,d):
 
 def stmts(x,d):
 	ret = ifstmts(x,d)
-	if ret != None: return 'stmts->ifstmts,' + ret
+	if ret != None: 
+		new_node = Node("stmts->ifstmts")
+		new_node.add_child(ret[1])
+		return 'stmts->ifstmts,' + ret[0], new_node
 
 	ret = whilestmts(x,d)
-	if ret != None: return 'stmts->whilestmts,' + ret
+	if ret != None:
+		new_node = Node("stmts->whilestmts")
+		new_node.add_child(ret[1])
+		return 'stmts->whilestmts,' + ret[0], new_node
 
 	ret = letstmts(x,d)
-	if ret != None: return 'stmts->letstmts,' + ret
+	if ret != None:
+		new_node = Node("stmts->letstmts")
+		new_node.add_child(ret[1])
+		return 'stmts->letstmts,' + ret[0], new_node
 
 	ret = printstmts(x,d)
-	if ret != None: return 'stmts->printstmts,' + ret
+	if ret != None:
+		new_node = Node("stmts->printstmts")
+		new_node.add_child(ret[1])
+		return 'stmts->printstmts,' + ret[0], new_node
 
 def printstmts(x,d):
 	if (len(x) >= 4):
 		if (x[0][0] == 'bracket-l' and x[-1][0] == 'bracket-r'):
 			if x[1][0] == 'keyword' and x[1][1] =='stdout':
 				ret = oper(x[2:-1],d)
-				if ret != None: return 'printstmts->[stdout oper],' + ret
+				if ret != None:
+					new_node = Node("printstmts->[stdout oper]")
+					new_node.add_child(ret[1])
+					return 'printstmts->[stdout oper],' + ret[0], new_node
 
 def ifstmts(x,d):
 	if len(x) >= 5:
@@ -186,13 +211,20 @@ def ifstmts(x,d):
 					ret2 = expr([x[3]],d)
 					ret3 = expr([x[4]],d)
 					if (ret1 != None and ret2 != None and ret3 != None):
-						return 'ifstmts->[if expr expr expr],' + ret1 + ret2 + ret3
+						new_node = Node("ifstmts->[if expr expr expr]")
+						new_node.add_child(ret1[1])
+						new_node.add_child(ret2[1])
+						new_node.add_child(ret3[1])
+						return 'ifstmts->[if expr expr expr],' + ret1[0] + ret2[0] + ret3[0], new_node
 
 				if (len(x) == 5):
 					ret1 = expr([x[2]],d)
 					ret2 = expr([x[3]],d)
 					if (ret1 != None and ret2 != None):
-						return 'ifstmts->[if expr expr],' + ret1 + ret2
+						new_node = Node("ifstmts->[if expr expr]")
+						new_node.add_child(ret1[1])
+						new_node.add_child(ret2[1])
+						return 'ifstmts->[if expr expr],' + ret1[0] + ret2[0], new_node
 
 def whilestmts(x,d):
 	if len(x) >= 5:
@@ -201,17 +233,26 @@ def whilestmts(x,d):
 				ret1 = expr([x[3]],d)
 				ret2 = exprlist(x[3:-1],d)
 				if (ret1 != None and ret2 != None):
-					return 'whilestmts->[while expr exprlist],' + ret1 + ret2
+					new_node = Node("whilestmts->[while expr exprlist]")
+					new_node.add_child(ret1[1])
+					new_node.add_child(ret2[1])
+					return 'whilestmts->[while expr exprlist],' + ret1[0] + ret2[0], new_node
 
 def exprlist(x,d):
 	ret1 = expr(x,d)
-	if ret1 != None: return 'exprlist->expr,' + ret1
+	if ret1 != None:
+		new_node = Node("exprlist->expr")
+		new_node.add_child(ret1[1])
+		return 'exprlist->expr,' + ret1[0], new_node
 
 	for i in range(0,len(x)):
 		ret1 = expr(x[i:],d)
 		ret2 = exprlist(x[:i],d)
 		if (ret1 != None and ret2 != None):
-			return 'exprlist->expr exprlist,' + ret1 + ret2
+			new_node = Node("exprlist->expr exprlist")
+			new_node.add_child(ret1[1])
+			new_node.add_child(ret2[1])
+			return 'exprlist->expr exprlist,' + ret1[0] + ret2[0], new_node
 
 
 def letstmts(x,d):
@@ -220,7 +261,10 @@ def letstmts(x,d):
 			if (x[1][0] == 'keyword' and x[1][1] == 'let'):
 				if (x[2][0] == 'bracket-l' and x[-2][0] == 'bracket-r'):
 					ret1 = varlist(x[3:-2],d)
-					if (ret1 != None): return 'letstmts->[let [varlist]],' + ret1
+					if (ret1 != None):
+						new_node = Node("letstmts->[let [varlist]]")
+						new_node.add_child(ret1[1])
+						return 'letstmts->[let [varlist]],' + ret1[0], new_node
 
 def varlist(x,d):
 	if (len(x) >= 4):
@@ -229,19 +273,27 @@ def varlist(x,d):
 				ret1 = name([x[1]],d)
 				ret2 = _type([x[2]],d)
 				if (ret1 != None and ret2 != None):
-					return 'varlist->[name type],' + ret1 + ret2
+					new_node = Node("varlist->[name type]")
+					new_node.add_child(ret1[1])
+					new_node.add_child(ret2[1])
+					return 'varlist->[name type],' + ret1[0] + ret2[0], new_node
 
 			if (len(x) >= 4):
 				ret1 = name([x[1]],d)
 				ret2 = _type([x[2]],d)
 				ret3 = varlist(x[4:],d)
 				if (ret1 != None and ret2 != None and ret3 != None):
-					return 'varlist->[name type] varlist,' + ret1 + ret2 + ret3
+					new_node = Node("varlist->[name type] varlist")
+					new_node.add_child(ret1[1])
+					new_node.add_child(ret2[1])
+					new_node.add_child(ret3[1])
+					return 'varlist->[name type] varlist,' + ret1[0] + ret2[0] + ret3[0], new_node
 
 def _type(x,d):
 	if x[0][0] == 'keyword':
 		if x[0][1] in ['bool','int','real', 'string']:
-			return 'type->'+x[0][1]+','
+			new_node = Node(x[0][1])
+			return 'type->'+x[0][1]+',',new_node
 
 '''
 
@@ -260,8 +312,6 @@ def parser(x):
 	#print "list of tokens:"
 	#print scanner_out[0], '\n'
 
-
-
 	if (scanner_out[1]):
 		#print "parsing..."
 		parser_in = scanner_out[0]
@@ -274,24 +324,19 @@ def parser(x):
 
 
 
-def tests2():
-
-	ts = [
-		'[[+ 1.1 1]]',
-		'[[+ 6 [* 1 5]]]']
-
-
-	print ''
+def test(ts,show_tree,show_derivation):
 	for t in ts:
-		print "-------"
+		if show_tree: print "-------"
 		out = parser(t)
 		if (out != None):
 			print t, "yes"
-			print_tree2(out[1])
+			if show_tree: print_tree2(out[1])
+			if show_derivation:
+				print "deriv:", out[0]
 		else: print t, "no"
-tests2()
 
-def tests():
+
+def tests(show_trees,show_derivations):
 	print "are any of the following in the grammar?"
 
 	# should all be yes
@@ -321,10 +366,7 @@ def tests():
 		]
 
 	print ''
-	for t in ts:
-		if (parser(t) != None):
-			print t, "yes"
-		else: print t, "no"
+	test(ts,show_trees,show_derivations)
 
 	# should all be no
 	ts = [
@@ -336,21 +378,19 @@ def tests():
 		]
 
 	print ''
-	for t in ts:
-		if (parser(t) != None):
-			print t, "yes"
-		else: print t, "no"
+	test(ts,show_trees,show_derivations)
 
 
 	# error/edge cases
 	# for finding bugs
 	ts = [
-		'[[while [= 5 x] [:= x [- x 1]]]]' # e.g. from class website
+		#'[[while [= 5 x] [:= x [- x 1]]]]' # e.g. from class website
 		]
 
 	print ''
-	for t in ts:
-		if (parser(t) != None):
-			print t, "yes"
-		else: print t, "no"
+	test(ts,show_trees,show_derivations)
+
+
+
+tests(True,False) #tests(show_tree?,show_derivation?)
 
